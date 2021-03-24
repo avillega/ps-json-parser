35f0f117c9f2562e3d1a093ec8eaf529bfb2041c
Andres Villegas <villegas.pelaez.andres@gmail.com>
2021-03-23 18:29:01 -0700

    Fix problem with Lazy implementation and implement jsonObject

--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,12 @@
 module Main where
 
 import Prelude
+
 import Control.Alt ((<|>))
 import Control.Lazy (fix)
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
-import Data.Tuple (Tuple)
+import Data.Tuple (Tuple(..))
 import Parser (Parser, charP, intP, manyP, sepByP, strLiteralP, tagP, wrappedP, wsP)
 
 data JsonValue
@@ -44,14 +45,36 @@ jsonString =
 jsonArray :: Parser JsonValue -> Parser JsonValue
 jsonArray p = JsonArray <$> wrappedP open (sepByP p wsAndComma) close
   where
-    open = (charP '[') *> (manyP wsP)
+  open = (charP '[') *> (manyP wsP)
 
-    close = (manyP wsP) <* (charP ']')
+  close = (manyP wsP) <* (charP ']')
 
-    wsAndComma = (manyP wsP) *> (charP ',') <* (manyP wsP)
+  wsAndComma = (manyP wsP) *> (charP ',') <* (manyP wsP)
+
+jsonObject :: Parser JsonValue -> Parser JsonValue
+jsonObject p = JsonObject <$> wrappedP open (sepByP objPair wsAndComma) close
+  where
+  open = (charP '{') *> (manyP wsP)
+
+  close = (manyP wsP) <* (charP '}')
+
+  wsAndComma = (manyP wsP) *> (charP ',') <* (manyP wsP)
+
+  objPair = do
+    str <- strLiteralP
+    _ <- (manyP wsP) *> (charP ':') *> (manyP wsP)
+    value <- p
+    pure (Tuple str value)
 
 jsonValue :: Parser JsonValue
 jsonValue =
   fix
     $ \p ->
-        (jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray p)
+        (manyP wsP)
+          *> ( jsonNull
+                <|> jsonBool
+                <|> jsonNumber
+                <|> jsonString
+                <|> jsonArray p
+                <|> jsonObject p
+            ) <* (manyP wsP)
